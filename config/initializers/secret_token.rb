1b22df2f9f45a7843e67a7d9fc3a66153bf1c6e0
Miguel Akira Ykeuti Meiga <miguel.akira@gmail.com>
2012-05-30 09:53:33 -0300

    commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stuffchecker::Application.config.secret_token = 'c15e897bd45ecdf2704c8f795c9b7a0652cab344a5288c4f907b6adc7eef2bb0224c921c5ebc5acd8af08b06d7e0985fee03ec85f2bcf6018ede8b65fb0936d1'
